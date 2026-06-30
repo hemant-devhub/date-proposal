6bd6c4bd6b06270f116bbab8a5b6c27da8d852da
Hemant Kumar <hemantk6240@gmail.com>
2026-06-30 21:20:08 +0530

    Countdown added Success.jsx

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -5,24 +5,26 @@ export default function Success() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCountdown((prev) => prev - 1);
+      setCountdown((prev) => {
+        if (prev <= 1) {
+          clearInterval(interval);
+
+          window.location.href =
+            "https://wa.me/919818612625?text=Yes%20❤️";
+
+          return 0;
+        }
+
+        return prev - 1;
+      });
     }, 1000);
 
-    const timer = setTimeout(() => {
-      window.location.href =
-        "https://wa.me/919818612625?text=Yes%20❤️";
-    }, 6000);
-
-    return () => {
-      clearInterval(interval);
-      clearTimeout(timer);
-    };
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <div className="page">
       <div className="card success">
-
         <img
           className="gif"
           src="https://media.giphy.com/media/26FLdmIp6wJr91JAI/giphy.gif"
@@ -31,16 +33,16 @@ export default function Success() {
 
         <h1>❤️ I Knew It!</h1>
 
-        <p>I knew you would choose <b>Yes.</b> 🥰</p>
-
         <p>
-          Tomorrow is our
-          <br />
-          <b>4 Month Anniversary ❤️</b>
+          I knew you would choose <b>Yes.</b>
         </p>
 
         <p>
-          Dress well 👗✨
+          Tomorrow is our <b>4 Month Anniversary ❤️</b>
+        </p>
+
+        <p>
+          Dress well...
           <br />
           We're going out tomorrow! 🌹
         </p>
@@ -48,11 +50,10 @@ export default function Success() {
         <h2>I Love You ❤️</h2>
 
         <div className="countdown">
-          Opening WhatsApp in
+          Opening WhatsApp in...
           <br />
           <span>{countdown}</span>
         </div>
-
       </div>
     </div>
   );
